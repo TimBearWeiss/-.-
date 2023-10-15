90806a142beaa02c324f2f55a5f0d364b39a3a76
Dmitrii Zverev <104170542+TimBearWeiss@users.noreply.github.com>
2023-10-15 14:59:09 +0300

    Add files via upload

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -6,6 +6,7 @@ import { Button } from "../ui/button/button";
 import { Circle } from "../ui/circle/circle";
 import { DELAY_IN_MS } from "../../constants/delays";
 import { ElementStates } from "../../types/element-states";
+import { convertString } from "./utils";
 
 type TArray = {
   value: string;
@@ -16,52 +17,15 @@ export const StringComponent: React.FC = () => {
   const [input, setInput] = useState("");
   const [stringArr, setString] = useState<Array<TArray>>([]);
   const [loader, setLoader] = useState(false);
+
   const onChangeInput = (evt: React.ChangeEvent<HTMLInputElement>) => {
     setInput(evt.target.value);
   };
 
-  const swap = (arr: string[], startPosition: number, endPosition: number) => {
-    const temp = arr[startPosition];
-
-    arr[startPosition] = arr[endPosition];
-    arr[endPosition] = temp;
-
-    return arr;
-  };
-
-  const convertString = async (arr: any) => {
-    setLoader(true);
-    const mid = Math.ceil(arr.length / 2);
-
-    for (let i = 0; i < mid; i++) {
-      let j = arr.length - 1 - i;
-
-      if (i !== j) {
-        arr[i].color = ElementStates.Changing;
-        arr[j].color = ElementStates.Changing;
-        setString([...arr]);
-
-        await new Promise((resolve) => setTimeout(resolve, DELAY_IN_MS));
-      }
-
-      swap(arr, i, j);
-
-      arr[i].color = ElementStates.Modified;
-      arr[j].color = ElementStates.Modified;
-
-      setString([...arr]);
-    }
-
-    setLoader(false);
-  };
-
   const submit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const newArr = input
-      .split("")
-      .map((value) => ({ value, color: ElementStates.Default }));
 
-    convertString(newArr);
+    convertString(input, setLoader, setString);
   };
 
   return (
